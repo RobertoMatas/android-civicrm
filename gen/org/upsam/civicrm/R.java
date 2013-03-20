02d4a4ade0d12b61c9bbf33ea8a89e556ebf48b1
Roberto Matas <pepinoglande@gmail.com>
2013-03-20 23:39:54 +0100

    Listado de contactos con pestañas/Detalle de contacto completo

--- gen/org/upsam/civicrm/R.java
+++ gen/org/upsam/civicrm/R.java
@@ -8,6 +8,10 @@
 package org.upsam.civicrm;
 
 public final class R {
+    public static final class array {
+        public static final int slide_individual_menu=0x7f040001;
+        public static final int slide_organization_menu=0x7f040000;
+    }
     public static final class attr {
         /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
 Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
@@ -122,73 +126,89 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int viewBehind=0x7f010002;
     }
     public static final class dimen {
-        public static final int list_padding=0x7f050001;
-        public static final int shadow_width=0x7f050002;
-        public static final int slidingmenu_offset=0x7f050000;
+        public static final int list_padding=0x7f060001;
+        public static final int shadow_width=0x7f060002;
+        public static final int slidingmenu_offset=0x7f060000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int shadow=0x7f020001;
-        public static final int social_person=0x7f020002;
+        public static final int ic_slide=0x7f020001;
+        public static final int shadow=0x7f020002;
+        public static final int social_person=0x7f020003;
     }
     public static final class id {
-        public static final int FrameLayout1=0x7f040005;
-        public static final int LinearLayout1=0x7f040006;
-        public static final int RelativeLayout1=0x7f04000c;
-        public static final int button1=0x7f040009;
-        public static final int button2=0x7f04000a;
-        public static final int button3=0x7f04000b;
-        public static final int contac_img=0x7f04000e;
-        public static final int contact_card=0x7f040013;
-        public static final int contact_list_item_layout=0x7f040012;
-        public static final int contact_type=0x7f04000f;
-        public static final int display_name=0x7f040010;
-        public static final int emails=0x7f04000d;
-        public static final int fullscreen=0x7f040003;
-        public static final int left=0x7f040000;
-        public static final int listView1=0x7f040007;
-        public static final int margin=0x7f040002;
-        public static final int menu_settings=0x7f040018;
-        public static final int phones=0x7f040011;
-        public static final int right=0x7f040001;
-        public static final int row_icon=0x7f040015;
-        public static final int row_title=0x7f040016;
-        public static final int selected_view=0x7f040004;
-        public static final int slidingmenumain=0x7f040017;
-        public static final int textView1=0x7f040008;
-        public static final int textView2=0x7f040014;
+        public static final int FrameLayout1=0x7f050007;
+        public static final int FrameLayout2=0x7f050008;
+        public static final int LinearLayout1=0x7f050009;
+        public static final int RelativeLayout1=0x7f050013;
+        public static final int button1=0x7f05000d;
+        public static final int button2=0x7f05000e;
+        public static final int button3=0x7f05000f;
+        public static final int contac_img=0x7f050014;
+        public static final int contact_card=0x7f050019;
+        public static final int contact_data=0x7f050017;
+        public static final int contact_list_item_layout=0x7f050018;
+        public static final int contact_type=0x7f050016;
+        public static final int container=0x7f050005;
+        public static final int display_address=0x7f050010;
+        public static final int display_city=0x7f050012;
+        public static final int display_name=0x7f050015;
+        public static final int display_supp_address=0x7f050011;
+        public static final int fragments=0x7f050006;
+        public static final int fullscreen=0x7f050003;
+        public static final int left=0x7f050000;
+        public static final int listView1=0x7f05000b;
+        public static final int margin=0x7f050002;
+        public static final int menu_settings=0x7f05001e;
+        public static final int progressBar1=0x7f05000a;
+        public static final int right=0x7f050001;
+        public static final int row_icon=0x7f05001b;
+        public static final int row_title=0x7f05001c;
+        public static final int selected_view=0x7f050004;
+        public static final int slidingmenumain=0x7f05001d;
+        public static final int textView1=0x7f05000c;
+        public static final int textView2=0x7f05001a;
     }
     public static final class integer {
-        public static final int num_cols=0x7f060000;
+        public static final int num_cols=0x7f070000;
     }
     public static final class layout {
         public static final int activity_contact_details=0x7f030000;
         public static final int activity_contact_list=0x7f030001;
         public static final int activity_main=0x7f030002;
-        public static final int contact_details_layout=0x7f030003;
-        public static final int contact_list_item=0x7f030004;
-        public static final int list=0x7f030005;
-        public static final int menu=0x7f030006;
-        public static final int row=0x7f030007;
-        public static final int slidingmenumain=0x7f030008;
+        public static final int contact_address_layout=0x7f030003;
+        public static final int contact_details_layout=0x7f030004;
+        public static final int contact_list_item=0x7f030005;
+        public static final int contact_tags_and_groups_layout=0x7f030006;
+        public static final int list_menu_individual=0x7f030007;
+        public static final int list_menu_organization=0x7f030008;
+        public static final int row=0x7f030009;
+        public static final int slide_menu_individual=0x7f03000a;
+        public static final int slide_menu_organization=0x7f03000b;
+        public static final int slidingmenumain=0x7f03000c;
     }
     public static final class menu {
-        public static final int activity_contact_details=0x7f090000;
-        public static final int activity_contact_list=0x7f090001;
-        public static final int activity_main=0x7f090002;
-        public static final int menu_list=0x7f090003;
+        public static final int activity_contact_details=0x7f0a0000;
+        public static final int activity_contact_list=0x7f0a0001;
+        public static final int activity_main=0x7f0a0002;
+        public static final int menu_list=0x7f0a0003;
     }
     public static final class string {
-        public static final int activity_button=0x7f070005;
-        public static final int app_name=0x7f070000;
-        public static final int cases_button=0x7f070006;
-        public static final int contact_image=0x7f070009;
-        public static final int contacts_button=0x7f070004;
-        public static final int hello_world=0x7f070001;
-        public static final int menu_settings=0x7f070002;
-        public static final int title_activity_contact_details=0x7f070008;
-        public static final int title_activity_contact_list=0x7f070003;
-        public static final int title_textview=0x7f070007;
+        public static final int activity_button=0x7f080005;
+        public static final int address=0x7f08000a;
+        public static final int app_name=0x7f080000;
+        public static final int cases_button=0x7f080006;
+        public static final int communication_preferences=0x7f08000b;
+        public static final int constituent_information=0x7f08000d;
+        public static final int contact_image=0x7f080009;
+        public static final int contacts_button=0x7f080004;
+        public static final int demographics=0x7f08000c;
+        public static final int hello_world=0x7f080001;
+        public static final int menu_settings=0x7f080002;
+        public static final int tags_and_groups=0x7f08000e;
+        public static final int title_activity_contact_details=0x7f080008;
+        public static final int title_activity_contact_list=0x7f080003;
+        public static final int title_textview=0x7f080007;
     }
     public static final class style {
         /** 
@@ -212,14 +232,14 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
+        public static final int AppTheme=0x7f090001;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int HoloTheme=0x7f080002;
+        public static final int HoloTheme=0x7f090002;
     }
     public static final class styleable {
         /** Attributes that can be used with a SlidingMenu.
